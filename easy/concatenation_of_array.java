1b66e92499573fccd7f8ca52e4e3e9bcac26078d
Sujith <198434303+Sujith-SM@users.noreply.github.com>
2025-10-03 02:46:09 +0530

    Update concatenation_of_array.java

--- easy/concatenation_of_array.java
+++ easy/concatenation_of_array.java
@@ -1,9 +1,7 @@
 # Problem: Concatenation of Array
-# Link: https://leetcode.com/problems/concatenation-of-array/
 # Difficulty: Easy
-# Tags: Array, Easy
 # Date: 2025-10-03
-# Status: Solved
+
 
 
 class Solution {
