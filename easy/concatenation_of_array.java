12fd3e9916bbd946ab25114754d91403ea2d4a5d
Sujith <198434303+Sujith-SM@users.noreply.github.com>
2025-10-03 02:33:52 +0530

    Update concatenation_of_array.java

--- easy/concatenation_of_array.java
+++ easy/concatenation_of_array.java
@@ -5,6 +5,15 @@
 # Date: 2025-10-03
 # Status: Solved
 
-class Solution:
-    def solve(self):
-        pass
+
+class Solution {
+    public int[] getConcatenation(int[] nums) {
+        int[] ans = new int[(nums.length)*2];
+        for(int i = 0; i < nums.length; i++){
+                    ans[i] = nums[i];
+                    ans[i+nums.length] = nums[i];
+            }
+        return ans;
+        
+    }
+}
