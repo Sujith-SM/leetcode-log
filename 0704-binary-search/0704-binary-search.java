fa7a7db9729acb60a39fd453f521a64502cdb4d2
Sujith <198434303+Sujith-SM@users.noreply.github.com>
2025-10-30 21:47:13 +0530

    Time: 0 ms (100%), Space: 45.7 MB (62.89%) - LeetHub

--- 0704-binary-search/0704-binary-search.java
+++ 0704-binary-search/0704-binary-search.java
@@ -1,20 +1,24 @@
 class Solution {
-    public int search(int[] nums, int target) {
-        int start = 0;
-        int end = nums.length-1;
-        int mid = 0;
-        while(start <= end){
-            mid = start + (end - start)/2;
-            if(nums[mid] == target){
-                return mid;
-            }
-            else if(nums[mid] > target){
-                end = mid -1;
-            }
-            else{
-                start = mid+1;
-            }
+    public int search(int[] nums, int target){
+        return pos(nums, target, 0, nums.length-1);
+    }
+
+    // recursive approach for binary search 
+    public int pos(int[] result, int pin, int start, int end){
+
+        if(start>end){
+            return -1;
         }
-        return -1;
+        
+        int mid = start + (end-start)/2;
+
+        if(result[mid] == pin){
+            return mid;
+        }
+        
+        if(result[mid] > pin){
+            return pos(result, pin, start, mid-1);
+        }
+        return pos(result, pin, mid+1, end);
     }
 }
